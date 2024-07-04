1f2a8cce9cd9e86eff35f79a61eea42a2300e1be
Baborikoks <baborikoks@gmail.com>
2024-07-04 22:07:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+ // Баборико Карина 6 группа
+ // Задание 1 по учебной практике 
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
